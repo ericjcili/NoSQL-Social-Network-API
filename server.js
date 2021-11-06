6dd51dc9e47fd6bb74fe8b89f12bcbae034dfc58
ericjcili <ericjcili@gmai.com>
2021-11-05 20:33:44 -0400

    connected to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const mongoose = require('mongoose');
+const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,12 +10,11 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost: ${PORT}`));
